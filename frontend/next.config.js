403eac0067bb0eaea6d258d78bc5fe2f0065bf21
Tarfua <117936402+Tarfua@users.noreply.github.com>
2025-03-06 21:51:49 +0200

    updated next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, 'src'),
-    };
-    return config;
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig; 
